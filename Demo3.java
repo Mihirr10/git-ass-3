a2aae2041c8f7fc73a8e83a4306fe04bc1945e08
mihir <mihirparmar031@gmail.com>
2023-02-20 11:41:15 +0530

    Revert "Ass-3, changed variable names in Demo3.java"
    This reverts commit bf6c9665b8ba57ac632e4cb0ea2c9a1f99c88cce.

--- Demo3.java
+++ Demo3.java
@@ -1,13 +1,13 @@
-class Demo3{  
+class PalindromeExample{  
     public static void main(String args[]){  
-     int rem,sum=0,temp;    
-     int num=454;//It is the number variable to be checked for palindrome  
+     int r,sum=0,temp;    
+     int n=454;//It is the number variable to be checked for palindrome  
      
-     temp=num;    
-     while(num>0){    
-      rem=num%10;  //getting remainder  
-      sum=(sum*10)+rem;    
-      num=num/10;    
+     temp=n;    
+     while(n>0){    
+      r=n%10;  //getting remainder  
+      sum=(sum*10)+r;    
+      n=n/10;    
      }    
      if(temp==sum)    
       System.out.println("palindrome number ");    
